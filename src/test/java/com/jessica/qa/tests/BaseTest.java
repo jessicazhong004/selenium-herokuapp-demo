d5df7e7037612ae2c21b1a3cf50893884e7fe7b6
jessicazhong004 <jessicazhong004@gmail.com>
2026-02-24 15:15:17 -0500

    ci: add Github Actions workflow for Selenium tests

--- src/test/java/com/jessica/qa/tests/BaseTest.java
+++ src/test/java/com/jessica/qa/tests/BaseTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -16,7 +17,17 @@ public abstract class BaseTest {
     @BeforeEach
     void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 在 CI 环境（GitHub Actions）里用 headless 模式跑
+        if ("true".equalsIgnoreCase(System.getenv("CI"))) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+        }
+
+        driver = new ChromeDriver(options);
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().setSize(new Dimension(1280, 800));
